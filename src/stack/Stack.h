a2e8e9acfe7f41bd8d918ffd93f396670e87170d
Highlight Clark <highlightz@163.com>
2015-08-18 09:55:09 +0800

    Update Stack.h

--- src/stack/Stack.h
+++ src/stack/Stack.h
@@ -1,3 +1,32 @@
+// Stack.h - an array based implementation of a stack
+
+#ifndef STACK_H
+#define STACK_H
+// Refer to
+// 'https://github.com/highlightz/Adjustable-Array/blob/master/AdjustableArray.h'
+#include "AdjustableArray.h"
+
+template < class DataType >
+class Stack
+{
+public:
+  Stack( );
+  void push( DataType elementToPush );
+  bool pop( DataType& poppedElement );
+  bool peek( DataType& topElement );
+  bool isEmpty( ) const;
+  void makeEmpty( );
+private:
+  int top;
+  AdjustableArray< DataType > elements;
+};
+
+template < class DataType >
+Stack< DataType >::Stack( )
+: top( -1 ), elements( 2 )
+{
+}
+
 template < class DataType >
 void Stack< DataType >::push( DataType elementToPush )
 {
@@ -14,23 +43,51 @@ bool Stack< DataType >::pop( DataType& poppedElement )
   if ( pop == -1 )
     return false;
     
-    poppedElement = elements[pop];
-    top--;
+  poppedElement = elements[pop];
+  top--;
+  
+  int trysize = elements.length( );
+  
+  while ( ( ( top + 1 ) <= ( trysize >> 2 ) ) && ( trysize > 2 ) )
+    trysize >>= 1;
     
-    int trysize = elements.length( );
+  if ( trysize < elements.length( ) )
+    try
+    {
+      elements.changeSize( trysize );
+    }
+    catch( ... )
+    {
+    }
     
-    while ( ( ( top + 1 ) <= ( trysize >> 2 ) ) && ( trysize > 2 ) )
-      trysize >>= 1;
-      
-    if ( trysize < elements.length( ) )
-      try
-      {
-        elements.changeSize( trysize );
-      }
-      catch( ... )
-      {
-        
-      }
-      
-      return true;
+  return true;
 }
+
+template < class DataType >
+bool Stack< DataType >::peek( DataType& topElement )
+{
+  if ( top == -1 )
+    return false;
+  topElement = elements[top];
+  return true;
+}
+
+template < class DataType >
+bool Stack< DataType >::isEmpty( ) const
+{
+  return top == -1;
+}
+
+template < class DataType >
+void Stack< DataType >::makeEmpty( )
+{
+  top = -1;
+  try
+  {
+    elements.changeSize( 2 );
+  }
+  catch ( ... )
+  {
+  }
+}
+#endif
